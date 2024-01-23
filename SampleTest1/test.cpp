02247da15b82afbccef26a685430b42252fda8c0
ashishkamblecctech <149309502+ashishkamblecctech@users.noreply.github.com>
2024-01-23 19:26:13 +0530

    Update test.cpp

--- SampleTest1/test.cpp
+++ SampleTest1/test.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include "Point3D.h"
 
+//Testing focuses on various aspects of the Point3D class
+
 TEST(Point3DTest, DefaultConstructor) {
+    //Point3DTest is test suite and DefaultConstructor is test case within that test suite
     Point3D point;
 
     EXPECT_FLOAT_EQ(point.x(), 0.0f);
@@ -9,6 +12,7 @@ TEST(Point3DTest, DefaultConstructor) {
 }
 
 TEST(Point3DTest, ParameterizedConstructor) {
+     //Point3DTest is test suite and ParameterizedConstructor is test case within that test suite
     Point3D point(1.0f, 2.0f);
 
     EXPECT_FLOAT_EQ(point.x(), 1.0f);
@@ -16,6 +20,7 @@ TEST(Point3DTest, ParameterizedConstructor) {
 }
 
 TEST(Point3DTest1, SettersAndGetters) {
+     //Point3DTest is test suite and SettersAndGetters is test case within that test suite
     Point3D point(3.0f, 4.0f);
     point.setX(3.0f);
     point.setY(4.0f);
@@ -25,6 +30,7 @@ TEST(Point3DTest1, SettersAndGetters) {
 }
 
 TEST(Point3DTest1, ParameterizedConstructor) {
+     //Point3DTest1 is test suite and ParameterizedConstructor is test case within that test suite
     Point3D point(1.0f, 2.0f, 3.0f);
 
     EXPECT_FLOAT_EQ(point.x(), 1.0f);
@@ -33,6 +39,7 @@ TEST(Point3DTest1, ParameterizedConstructor) {
 }
 
 TEST(Point3DTest, SettersAndGetters) {
+     //Point3DTest1 is test suite and SettersAndGetters is test case within that test suite
     Point3D point(3.0f, 4.0f);
     point.setX(3.0f);
     point.setY(4.0f);
